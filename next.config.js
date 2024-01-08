016dcb5d3b2d5b88e517d91f3f705e996254ce4e
Situn09 <106417684+Situn09@users.noreply.github.com>
2024-01-08 23:52:28 +0530

    git page set up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
+
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
+  
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
-module.exports = nextConfig
+export default nextConfig;
